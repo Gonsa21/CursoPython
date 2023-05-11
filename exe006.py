009718a27c08117c84aaeeaa561d6a57b74a164e
Gonsa21 <114664188+Gonsa21@users.noreply.github.com>
2023-05-11 11:33:11 -0300

    Atividade 006

---
+++ exe006.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um numero: '))
+print('O dobro de {} é {}.'.format(n1, n1*2))
+print('o tripo de {} é {}.'.format(n1, n1*3))
+print('A Raiz quadrada de {} é {:.2f}.'.format(n1, n1**(1/2)))
