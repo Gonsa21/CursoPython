c2d17a274381f1ac6b8787b1adf1c8a1dd6c3cd7
Gonsa21 <114664188+Gonsa21@users.noreply.github.com>
2023-05-11 11:29:19 -0300

    Create exe002.py
    Atividade 2

---
+++ exe002.py
@@ -0,0 +1,4 @@
+num1 = int(input('Digite sua idade: '))
+num2 = int(input('Digite a idade do seu crush: '))
+soma = num1 + num2
+print('Sua idade {}, somada a idade do seu crush, {}, resulta na idade do casal em {} anos.'.format(num1, num2, soma))
