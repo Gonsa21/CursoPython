8a1b4f99771ff9217fde1b7fc168d33439448320
Gonsa21 <114664188+Gonsa21@users.noreply.github.com>
2023-05-12 06:21:17 -0300

    Create exe011.py

---
+++ exe011.py
@@ -0,0 +1,4 @@
+larg = float(input('Qual a altura da parede: '))
+cump = float(input('Qual o cumprinto da parede: '))
+print('Sua parede tem a dimensão de {}x{}, assimsua area é de {}m².'.format(larg,cump, larg*cump))
+print('Nessa parede sera ultilizado {} litros de tinta.'.format(larg * cump / 2))
