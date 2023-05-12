2a823067674d9083075de1ea792a782f4e194894
Gonsa21 <114664188+Gonsa21@users.noreply.github.com>
2023-05-12 05:53:16 -0300

    Atividade 008

---
+++ exe008.py
@@ -0,0 +1,13 @@
+med = float(input('Uma distancia em metros: '))
+km = med * 0.001
+hm = med * 0.01
+dam = med * 0.1
+dm = med * 10
+cm = med * 100
+mm = med * 1000
+print('A medida de {}m corresponde a: {}km'.format(med, km))
+print('A medida de {}m corresponde a: {}hm'.format(med, hm))
+print('A medida de {}m corresponde a: {}dam'.format(med, dam))
+print('A medida de {}m corresponde a: {}dm'.format(med, dm))
+print('A medida de {}m corresponde a: {}cm'.format(med, cm))
+print('A medida de {}m corresponde a: {}mm'.format(med, mm))
