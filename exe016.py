875f2f6db841b7458f98116cd0af2ad848dc1957
Gonsa21 <114664188+Gonsa21@users.noreply.github.com>
2023-05-20 22:19:42 -0300

    Atividade 16

---
+++ exe016.py
@@ -0,0 +1,3 @@
+num = float( input('Digite um numero com casas decimais com ponto: '))
+resut = round(num)
+print('Arredondando o numero {} para o numero inteiro mais proximo é {}.'.format(num, resut))
